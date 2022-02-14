3ca5449e3483f99f403bea90f5604f4d3ec87581
Samet Dogru <ahmetsametdogru@gmail.com>
2022-02-14 04:32:37 +0300

    date format fix

--- VideoGamesApp/Base/BaseCollectionView/BaseCollectionViewCell.swift
+++ VideoGamesApp/Base/BaseCollectionView/BaseCollectionViewCell.swift
@@ -48,7 +48,7 @@ class BaseCollectionViewCell: UICollectionViewCell, Reusable {
     
     func getDate(date: String) -> Date? {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        dateFormatter.dateFormat = "yyyy-MM-dd"
         dateFormatter.timeZone = TimeZone.current
         dateFormatter.locale = Locale.current
         return dateFormatter.date(from: date) // replace Date String
